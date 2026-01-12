aaa19198742e8d3171453f404c6dbfa18ceca7fb
Manuel <manualexandro09gmail.com>
2026-01-12 11:11:15 -0500

    cree la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LogsModule } from './logs/logs/logs.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [LogsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+    type:'sqlite',
+    database:'database.db',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize:true,
+    }),
+    LogsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService  ],
 })
 export class AppModule {}
